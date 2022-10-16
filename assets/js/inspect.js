964f76e5fb1303be90a95f327fc2eaa75ccbec33
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-10-15 22:57:52 -0400

    Game List is now appearing as a dropdown menu for any pokemon that is searched for... figuring out a way to make the dropdown menu into a scrollbar instead to avoid overflow on the page

--- assets/js/inspect.js
+++ assets/js/inspect.js
@@ -1,18 +1,26 @@
 // (kaileb) I am assuming that for locations we were meaning routes that this pokemon can be encountered on so that is what I am putting here.
 
+// El & ID list
+let gameListDropdownID = $('#game-list-dropdown')
+
 // get searched for pokemon's data from local storage and convert back into an object
 let currentPokemonData = localStorage.getItem('currentPokemonData');
 currentPokemonData = JSON.parse(currentPokemonData);
 
 // create an arrow function to handle data for Game List | This is called just like a normal function would be
 const handleGameList = () => {
-    console.log(currentPokemonData.game_indices);
-
-    for (var i = 0; i < currentPokemonData.game_indices.length; i++) {
-        
-    }
+    console.log(currentPokemonData.game_indices[0].version.name);
+    // change html for dropdown and for find out what games "this pokemon" appears in to match name of searched for pokemon
+        $(currentPokemonData.game_indices).each(function(i){
+            let button = document.createElement('button');
+            button.textContent =`${currentPokemonData.game_indices[i].version.name.toUpperCase()}`
+            button.classList.add('button', 'dropdown-item', 'dropdown-border', 'pkmn-yellow-background', 'pkmn-black-text', 'is-size-6', 'has-text-center')
+            gameListDropdownID[0].appendChild(button);
+    });
 
 }
 
 
 handleGameList();
+
+let html = `<button class="button dropdown-item dropdown-border pkmn-yellow-background pkmn-black-text is-size-6">Pokemon Yellow</button>`

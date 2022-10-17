7a68cae43d1e625bdbd40685934aec222aa34aea
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-10-17 17:18:48 -0400

    updated js to dynamically create HTML

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,8 +1,11 @@
+// declare any ID's || Elements
+let gameImageID = $("#game-img");
+let gameNameID = $('#game-name')
+let gameDetailsID = $('#game-details');
 
-gameName = "pokemon-y";
-gamesAPI = "https://api.rawg.io/api/games/" + gameName + "?key=986d608da5c14059809c05240f4ae2e9&dates=2019-09-01,2019-09-30&platforms=18,1,7"
-
-
+// API Data:
+let gameName = "pokemon-y";
+const gamesAPI = "https://api.rawg.io/api/games/" + gameName + "?key=986d608da5c14059809c05240f4ae2e9&dates=2019-09-01,2019-09-30&platforms=18,1,7"
 
 fetch(gamesAPI)
     .then(function (response) {
@@ -12,22 +15,28 @@ fetch(gamesAPI)
         console.log(response);
 
         //Game name
-        $("#game-name").text(response.name)
+        let gameNameHTML = $(`<h4 class="title is-2 pkmn-yellow-text">${response.name}</h4>`);
+        gameNameID.append(gameNameHTML);
 
         //Game img
-        $("#game-img").html("<img src=" + response.developers[0].image_background + ">");
+        gameImageID.html(`<img src="${response.developers[0].image_background}">`);
                    
         //description of game
-        $("#summary").text("Summary: " + response.description_raw);
+        let summaryHTML = $(`<p><strong class="pkmn-yellow-text is-size-4">Summary:</strong> <span class="pkmn-white-text">${response.description_raw}</span></p>`);
+        gameDetailsID.append(summaryHTML);
         
-        //plataform
-        $("#platform").text("Plataform: " + response.platforms[0].platform.name);
+        //platform
+        let platformHTML = $(`<p><strong class="pkmn-yellow-text is-size-4">Platform: </strong><span class="pkmn-white-text">${response.platforms[0].platform.name}</span></p>`);
+        summaryHTML.append(platformHTML);
 
-        //developer
-        $("#developers").text("Developer: " + response.developers[0].name);
 
         //released
-        $("#released-date").text("Released Date: " + response.released);
+        let releaseDateHTML = $(`<p><strong class="pkmn-yellow-text is-size-4">Release Date: </strong><span class="pkmn-white-text">${response.released}</span></p>`);
+        platformHTML.append(releaseDateHTML);
+
+        //developer
+        let developerHTML = $(`<p><strong class="pkmn-yellow-text is-size-4">Developer: </strong><span class="pkmn-white-text">${response.developers[0].name}</span></p>`);
+        releaseDateHTML.append(developerHTML);
     
       }
     );
